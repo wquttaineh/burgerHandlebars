327e12a8d40714695a5ebb4ca42e108893a59dfb
Fiona Ballard <fionaballard@Fiona-2.local>
2016-11-30 21:25:41 -0500

    yo

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 var connection;
 if(process.env.JAWSDB_URL){
-	connection = process.env.JAWSDB_URL;
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else{ 
 	connection = mysql.createConnection({
     host: 'localhost',
