4a55598c8279dcc3230ed60a88a6bf6a19d22e84
Fiona Ballard <fionaballard@Fiona-2.local>
2016-11-30 21:05:12 -0500

    heroku db jaws installation

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,15 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
+var connection;
+if(process.env.JAWSDB_URL){
+	connection = process.env.JAWSDB_URL;
+}else{ 
+	connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'root',
     database: 'burgers_db'
-});
+})
+}
 
 
 connection.connect(function(err) {
